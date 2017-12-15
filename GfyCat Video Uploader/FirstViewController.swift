3b0bcb878d56159d76e12515bb80c999673197d0
Tyler Hostager <tyhostager@gmail.com>
2017-12-14 20:39:23 -0600

    Update: add functionality and methods

--- GfyCat Video Uploader/FirstViewController.swift
+++ GfyCat Video Uploader/FirstViewController.swift
@@ -8,18 +8,47 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet var mainView: UILabel!
+    @IBOutlet var selectFileBtn: UIButton!
+    @IBOutlet var uploadBtn: UIButton!
+    @IBOutlet var imgView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
+    
+    @IBAction func selectFileBtnPressed(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        self.present(pickerController, animated: true, completion: nil)
+    }
+    
+    @IBAction func uploadBtnPressed(_ sender: Any) {
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        imgView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
 
+    func imgUploadRequest() -> Void {
+        var uploadRequestURL = ""
+        /*
+         TODO: figure out web request to online-convert.com
+         */
+        print("Upload request: \"\(uploadRequestURL)\"")
+        
+        
+    }
 }
 
--- GfyCat Video Uploader/WebRequestObj.swift
+++ GfyCat Video Uploader/WebRequestObj.swift
@@ -10,15 +10,30 @@ import UIKit
 
 class WebRequestObj: NSObject {
 
+    let kDefaultMediaType = "image"
+    
+    var mediaType: String!
     
     /// Default constructor
     override init() {
-        
+        self.mediaType = kDefaultMediaType
     }
     
     
-    init(_ id: WebRequestObj?) {
-        
+    /// Constructs a web request object using a previously created web request
+    ///
+    /// - Parameter webRequest: Another web request object in which the current
+    ///                         web request should duplicate
+    init(withWebRequest webRequest: WebRequestObj!) {
+        if webRequest != nil {
+            self.mediaType = webRequest!.mediaType
+        }
+    }
+    
+    init(withMediaType mediaType: String!, withRequestHeader requestHeader: String!) {
+        if mediaType.isEmpty {
+            
+        }
     }
     
     
