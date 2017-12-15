ae153036ec9ff312a0a97885cd136a7a10a6b7a2
Tyler Hostager <tyhostager@tyler-hs-macbook-pro.local>
2017-12-14 18:38:14 -0600

    Add: web request object to specify upload request parameters depending on file type

---
+++ GfyCat Video Uploader/WebRequestObj.swift
@@ -0,0 +1,25 @@
+//
+//  WebRequestObj.swift
+//  GfyCat Video Uploader
+//
+//  Created by Tyler hostager on 12/14/17.
+//  Copyright © 2017 Tyler hostager. All rights reserved.
+//
+
+import UIKit
+
+class WebRequestObj: NSObject {
+
+    
+    /// Default constructor
+    override init() {
+        
+    }
+    
+    
+    init(_ id: WebRequestObj?) {
+        
+    }
+    
+    
+}
